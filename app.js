57c5ff74a0793e767eda6f4230c961cb3af7d2ee
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-05 15:30:41 -0500

    add login page and template

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const middleware = require('./middleware');
 app.set('View engine', 'pug');
 app.set('views', 'views');
 
+// Routes
+const loginRoute = require('./routes/loginRoutes');
+app.use('/login', loginRoute);
+
 app.get('/', middleware.requireLogin, (req, res, next) => {
   const payload = {
     pageTitle: 'Home',
