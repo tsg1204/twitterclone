bc4d6c6ad8f4a744132ea35a5002c6edbfb99d02
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-05 15:05:01 -0500

    middleware added to check for user login

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const middleware = require('./middleware');
 
 app.set('View engine', 'pug');
 app.set('views', 'views');
 
-app.get('/', (req, res, next) => {
-  res.status(200).render('home.pug');
+app.get('/', middleware.requireLogin, (req, res, next) => {
+  const payload = {
+    pageTitle: 'Home',
+  };
+
+  res.status(200).render('home.pug', payload);
 });
 
 const server = app.listen(port, () => {
