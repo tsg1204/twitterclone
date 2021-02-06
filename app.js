523d2da6c62fd0b9231ff3ad4927f9010db32c8d
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-06 13:53:21 -0500

    added login func

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const middleware = require('./middleware');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoDB = require('./database');
+const session = require('express-session');
+const sessionSecret = require('./dev/keys');
 
 mongoDB.connectDB();
 
@@ -13,6 +15,14 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    secret: sessionSecret.sessionSecret,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
@@ -24,6 +34,7 @@ app.use('/register', registerRoute);
 app.get('/', middleware.requireLogin, (req, res, next) => {
   const payload = {
     pageTitle: 'Home',
+    userLoggedIn: req.session.user,
   };
 
   res.status(200).render('home.pug', payload);
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 const User = require('../schemas/UserSchema');
+const bcrypt = require('bcrypt');
 
 app.set('View engine', 'pug');
 app.set('views', 'views');
@@ -26,26 +27,29 @@ router.post('/', async (req, res, next) => {
       $or: [{ username: username }, { email: email }],
     }).catch((error) => {
       console.log(error);
-      payload.erroMessage = 'Something went wrong with DB connection';
+      payload.errorMessage = 'Something went wrong with DB connection';
       res.status(200).render('register.pug', payload);
     });
 
     if (user === null) {
       const data = req.body;
 
+      data.password = await bcrypt.hash(password, 10);
+
       User.create(data).then((user) => {
-        console.log(user);
+        req.session.user = user;
+        return res.redirect('/');
       });
     } else {
       if (email === user.email) {
-        payload.erroMessage = 'Email already in use';
+        payload.errorMessage = 'Email already in use';
       } else {
-        payload.erroMessage = 'Username already in use';
+        payload.errorMessage = 'Username already in use';
       }
       res.status(200).render('register.pug', payload);
     }
   } else {
-    payload.erroMessage = 'Make sure each field has a valid value';
+    payload.errorMessage = 'Make sure each field has a valid value';
     res.status(200).render('register.pug', payload);
   }
 });
