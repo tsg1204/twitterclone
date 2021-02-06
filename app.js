fd97ac80d2112b49721b5e23943081702e3cc5c6
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-06 11:02:38 -0500

    added register page

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3003;
 const middleware = require('./middleware');
+const path = require('path');
 
 app.set('View engine', 'pug');
 app.set('views', 'views');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
 const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
+
 app.use('/login', loginRoute);
+app.use('/register', registerRoute);
 
 app.get('/', middleware.requireLogin, (req, res, next) => {
   const payload = {
---
+++ routes/registerRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+app.set('View engine', 'pug');
+app.set('views', 'views');
+
+router.get('/', (req, res, next) => {
+  res.status(200).render('register.pug');
+});
+
+module.exports = router;
