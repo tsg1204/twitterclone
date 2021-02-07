518c956068fbb5c425fcb2e8bb77b1d7c4309eed
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-06 19:03:31 -0500

    added post display on the page func

--- public/js/common.js
+++ public/js/common.js
@@ -15,3 +15,37 @@ $('#postTextarea').keyup((event) => {
 
   submitButton.prop('disabled', false);
 });
+
+$('#submitPostButton').click((event) => {
+  const button = $(event.target);
+  const textbox = $('#postTextarea');
+  const data = {
+    content: textbox.val(),
+  };
+
+  $.post('/api/posts', data, (postData, status, xhr) => {
+    const html = createPostHtml(postData);
+    $('.postsContainer').prepend(html);
+    textbox.val('');
+    button.prop('disabled', true);
+  });
+});
+
+const createPostHtml = (postData) => {
+  const postedBy = postData.postedBy;
+
+  return `<div class="post">
+            <div class="mainContentContainer">
+              <div class="userImageContainer">
+                <img src="${postedBy.profilePic}" >
+              </div>
+              <div class="postContentContainer">
+                <div class="header"></div>
+                <div class="postBody">
+                  <span>${postData.content}</span>
+                </div>
+                <div class="postFooter"></div>
+              </div>
+            </div>
+          </div>`;
+};
