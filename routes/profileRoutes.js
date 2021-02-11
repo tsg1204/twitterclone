5ac8d88ac037c06821e6e471657fa5820d274295
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-11 12:34:27 -0500

    added profile page

---
+++ routes/profileRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+const User = require('../schemas/UserSchema');
+const bcrypt = require('bcrypt');
+
+router.get('/:id', (req, res, next) => {
+  const payload = {
+    pageTitle: req.session.user.username,
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+    profileUser: req.session.user,
+  };
+
+  res.status(200).render('profilePage.pug', payload);
+});
+
+module.exports = router;
