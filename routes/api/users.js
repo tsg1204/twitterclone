34e017da5873d11594e96354be40d0279f7cdce1
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-20 16:20:13 -0500

    fixed upload image

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,8 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const User = require('../../schemas/UserSchema');
 const Post = require('../../schemas/PostSchema');
+const path = require('path');
+const fs = require('fs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -71,7 +73,25 @@ router.post(
       console.log('No file uploaded with ajax request.');
       return res.sendStatus(400);
     }
-    res.sendStatus(200);
+
+    const filePath = `/uploads/images/${req.file.filename}.png`;
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, `../../${filePath}`);
+
+    fs.rename(tempPath, targetPath, async (error) => {
+      if (error !== null) {
+        console.log(error);
+        return res.sendStatus(400);
+      }
+
+      req.session.user = await User.findByIdAndUpdate(
+        req.session.user._id,
+        { profilePic: filePath },
+        { new: true }
+      );
+
+      res.sendStatus(204);
+    });
   }
 );
 
