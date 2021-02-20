aeda8a67ea5ba63c22e3869d9d17d3ef76b13a52
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-20 11:19:11 -0500

    follow page added

--- public/js/common.js
+++ public/js/common.js
@@ -143,12 +143,20 @@ $(document).on('click', '.followButton', (event) => {
         return;
       }
 
+      let difference = 1;
       if (data.following && data.following.includes(userId)) {
         button.addClass('following');
         button.text('Following');
       } else {
         button.removeClass('following');
         button.text('Follow');
+        difference = -1;
+      }
+
+      let followersLabel = $('#followersValue');
+      if (followersLabel.length !== 0) {
+        let followersText = parseInt(followersLabel.text());
+        followersLabel.text(followersText + difference);
       }
     },
   });
