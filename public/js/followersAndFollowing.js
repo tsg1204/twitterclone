5ef9bf542a2b49f5b0274ab6758a61e5d0bd7597
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-20 12:42:05 -0500

    styled followers page

--- public/js/followersAndFollowing.js
+++ public/js/followersAndFollowing.js
@@ -8,16 +8,41 @@ $(document).ready(() => {
 
 const loadFollowers = () => {
   $.get(`/api/users/${profileUserId}/followers`, (results) => {
-    outputUsers(results, $('.resultsContainer'));
+    outputUsers(results.followers, $('.resultsContainer'));
   });
 };
 
 const loadFollowing = () => {
   $.get(`/api/users/${profileUserId}/following`, (results) => {
-    outputUsers(results, $('.resultsContainer'));
+    outputUsers(results.following, $('.resultsContainer'));
   });
 };
 
-const outputUsers = (data, container) => {
-  console.log(data);
+const outputUsers = (results, container) => {
+  container.html('');
+
+  results.forEach((result) => {
+    const html = createUserHtml(result, true);
+    container.append(html);
+  });
+
+  if (results.length === 0) {
+    container.append("<span class='noResults'>No results found</span>");
+  }
+};
+
+const createUserHtml = (userData, showFollowButton) => {
+  const name = userData.firstName + ' ' + userData.lastName;
+
+  return `<div class='user'>
+              <div class='userImageContainer'>
+                  <img src='${userData.profilePic}'>
+              </div>
+              <div class='userDetailsContainer'>
+                  <div class='header'>
+                      <a href='/profile/${userData.username}'>${name}</a>
+                      <span class='username'>@${userData.username}</span>
+                  </div>
+              </div>
+          </div>`;
 };
