4e4b7f9e8bfcb628b5eb8e6dc68f746ae79ba38f
Tatiana Grigorieva <tsg1204@gmail.com>
2021-02-19 19:19:19 -0500

    styling profile

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const User = require('../schemas/UserSchema');
 const bcrypt = require('bcrypt');
 
-router.get('/:id', (req, res, next) => {
+router.get('/', (req, res, next) => {
   const payload = {
     pageTitle: req.session.user.username,
     userLoggedIn: req.session.user,
@@ -15,5 +15,35 @@ router.get('/:id', (req, res, next) => {
 
   res.status(200).render('profilePage.pug', payload);
 });
+
+router.get('/:username', async (req, res, next) => {
+  const payload = await getPayload(req.params.username, req.session.user);
+  console.log('Payload: ', payload);
+
+  res.status(200).render('profilePage.pug', payload);
+});
+
+const getPayload = async (username, userLoggedIn) => {
+  let user = await User.findOne({ username: username });
+
+  if (user === null) {
+    user = await User.findById(username);
+
+    if (user === null) {
+      return {
+        pageTitle: 'User not found',
+        userLoggedIn: userLoggedIn,
+        userLoggedInJs: JSON.stringify(userLoggedIn),
+      };
+    }
+  }
+
+  return {
+    pageTitle: user.username,
+    userLoggedIn: userLoggedIn,
+    userLoggedInJs: JSON.stringify(userLoggedIn),
+    profileUser: user,
+  };
+};
 
 module.exports = router;
