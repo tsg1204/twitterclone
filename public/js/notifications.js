c73adfbacf601d833de43b258d3b4dbff97cc7a9
Tatiana Grigorieva <tsg1204@gmail.com>
2021-03-13 15:29:05 -0500

    added notification html

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -1,5 +1,29 @@
 $(document).ready(() => {
   $.get('/api/notifications', (data) => {
-    console.log(data);
+    outptNotificationList(data, $('.resultsContainer'));
   });
 });
+
+const outptNotificationList = (notifications, container) => {
+  notifications.forEach((notification) => {
+    const html = createNotificationHtml(notification);
+    container.append(html);
+  });
+
+  if (notification.length === 0) {
+    container.append('<span class="noResults">Nothing to show.</span>');
+  }
+};
+
+const createNotificationHtml = (notification) => {
+  const userFrom = notification.userFrom;
+
+  return `<a href='#' class='resultListItem notification'>
+          <div class='resultsImageContainer'>
+            <img src='${userFrom.profilePic}'>
+          </div>
+          <div class='resultsDetailsContainer ellipsis'>
+            <span class='ellipsis'>${'this is the text'}</span>
+          </div>
+        </a>`;
+};
