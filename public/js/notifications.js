33876b571cc4a3f0d59db11bbad951b0efc22512
Tatiana Grigorieva <tsg1204@gmail.com>
2021-03-13 16:51:37 -0500

    display notifications

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -17,13 +17,56 @@ const outptNotificationList = (notifications, container) => {
 
 const createNotificationHtml = (notification) => {
   const userFrom = notification.userFrom;
+  const text = getNotificationText(notification);
+  const href = getNotificationUrl(notification);
+  const className = notification.opened ? '' : 'active';
 
-  return `<a href='#' class='resultListItem notification'>
+  return `<a href='${href}' class='resultListItem notification ${className}'>
           <div class='resultsImageContainer'>
             <img src='${userFrom.profilePic}'>
           </div>
           <div class='resultsDetailsContainer ellipsis'>
-            <span class='ellipsis'>${'this is the text'}</span>
+            <span class='ellipsis'>${text}</span>
           </div>
         </a>`;
+};
+
+const getNotificationText = (notification) => {
+  const userFrom = notification.userFrom;
+
+  if (!userFrom.firstName || !userFrom.lastName) {
+    return alert('user from data not populated');
+  }
+
+  const userFromName = `${userFrom.firstName} ${userFrom.lastName}`;
+
+  let text;
+
+  if (notification.notificationType === 'retweet') {
+    text = `${userFromName} retweeted one of your posts`;
+  } else if (notification.notificationType === 'postLike') {
+    text = `${userFromName} liked one of your posts`;
+  } else if (notification.notificationType === 'reply') {
+    text = `${userFromName} replied to one of your posts`;
+  } else if (notification.notificationType === 'follow') {
+    text = `${userFromName} followed you`;
+  }
+
+  return `<span class='ellipsis'>${text}</span>`;
+};
+
+const getNotificationUrl = (notification) => {
+  let url = '#';
+
+  if (
+    notification.notificationType === 'retweet' ||
+    notification.notificationType === 'postLike' ||
+    notification.notificationType === 'reply'
+  ) {
+    url = `/posts/${notification.entityId}`;
+  } else if (notification.notificationType === 'follow') {
+    url = `/profile/${notification.entityId}`;
+  }
+
+  return url;
 };
